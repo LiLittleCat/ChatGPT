5aeba6f20c69eb4582c9c2f88e69769f805d279a
Yi Liu <luoyukongchan@outlook.com>
2023-04-11 22:06:42 +0800

    Feature: Add GPT-4 models(#5), Add custom host(#8) (#11)

--- src/main/java/com/lilittlecat/chatgpt/offical/entity/Constant.java
+++ src/main/java/com/lilittlecat/chatgpt/offical/entity/Constant.java
@@ -18,6 +18,7 @@ public class Constant {
      * <p>
      * see: <a href=https://platform.openai.com/docs/api-reference/chat/create>https://platform.openai.com/docs/api-reference/chat/create</a>
      */
-    public static final String CHAT_COMPLETION_API_URL = "https://api.openai.com/v1/chat/completions";
+    public static final String DEFAULT_CHAT_COMPLETION_API_URL = "https://api.openai.com/v1/chat/completions";
     public static final String DEFAULT_USER = "user";
+    public static final Model DEFAULT_MODEL = Model.GPT_3_5_TURBO;
 }
